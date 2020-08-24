e7605fd0ec3c2c7ad762ee567590dba6077a61aa
SugarTea2899 <quangthien.tiit@gmail.com>
2020-08-24 20:34:42 +0700

    ! Loại môn tin học cơ sở

--- gpa.js
+++ gpa.js
@@ -7,7 +7,8 @@ for (i = 1, row = null; row = table.rows[i]; i++) {
         const subjectName = row.cells[1].innerText;
         if (subjectName.includes("Thể dục 1") || subjectName.includes("Thể dục 2")
             || subjectName.includes("Anh văn 1") || subjectName.includes("Anh văn 2") 
-            || subjectName.includes("Anh văn 3") || subjectName.includes("Anh văn 4") || subjectName.includes("Giáo dục quốc phòng")){
+            || subjectName.includes("Anh văn 3") || subjectName.includes("Anh văn 4") || subjectName.includes("Giáo dục quốc phòng") 
+			|| subjectName.includes("Tin học")){
                 continue;
             }
 				
